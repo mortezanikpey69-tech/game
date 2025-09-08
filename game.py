76475a4918bbdc199757f86a16ae4934e2b069e8
morteza nikpey <morteza.nikpey69@gmail.com>
2025-09-08 11:56:55 +0330

    Initial commit

---
+++ game.py
@@ -0,0 +1,19 @@
+number=first= input("please input one number :")
+number=int(number)
+first=int(first)
+#number= number*2
+number *=2
+#number=number+8
+number +=8
+#number=number+first
+number +=first
+#number=number-2
+number -=2
+#number=number/3
+number /=3
+#number=number-first
+number -=first
+#number=number*4
+number *=4
+print("your number is 8?")
+print(number)
